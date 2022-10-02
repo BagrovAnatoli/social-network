9ec7bf9f9f2768a2ce147200ed4f19e349d17ce7
BagrovAnatoli <bagrov.anatoli@gmail.com>
2022-10-02 16:52:02 +0300

    13 lesson

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import './Header.css';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img className="logo" src="https://cs10.pikabu.ru/post_img/2020/09/05/9/159931529822441155.jpg" alt="angel"/>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,18 @@
+const Profile = () => {
+    return (
+        <div className="content">
+            <div className="content__img"></div>
+            <div>ava + description</div>
+            <div>
+            my posts
+            <div>new post</div>
+            <div>
+                <div>post 1</div>
+                <div>post 2</div>
+            </div>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
