a573f10dc805a9041e7cd87eef7293f80de666e2
BagrovAnatoli <bagrov.anatoli@gmail.com>
2022-10-04 12:25:36 +0300

    16 lesson

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,13 +1,13 @@
 import s from './Post.module.css';
 
-const Post = ({message}) => {
+const Post = ({message, likesCount}) => {
     return (
         <div>
             <div className={s.item}>
                 <img className={s.ava} src="https://avatars.mds.yandex.net/i?id=019b41db423c7fa7dcd43c6883f07a9f-3727926-images-thumbs&n=13"></img>
                 { message }
                 <div>
-                    <span>like</span>
+                    <span>like {likesCount}</span>
                 </div>
             </div>
         </div>
