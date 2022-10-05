f891cba5ee3a90827a28791cbbd479f79a56cf41
BagrovAnatoli <bagrov.anatoli@gmail.com>
2022-10-05 18:08:59 +0300

    24 lesson

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -23,19 +23,28 @@ const Message = ({ message }) => {
 }
 
 const Dialogs = () => {
+    const dialogsData = [
+        {id: '1', name: 'Galina'},
+        {id: '2', name: 'Vladimir'},
+        {id: '3', name: 'Anna'},
+        {id: '4', name: 'Elena'},
+        {id: '5', name: 'Kirill'},
+        {id: '6', name: 'Olga'},
+    ];
+
+    const messagesData = [
+        {id: '1', message: 'Hi'},
+        {id: '2', message: 'How is you it-kamasutra?'},
+        {id: '3', message: 'Good'},
+    ];
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogs_items}>                   
-                <DialogItem id="1" name="Sasha"/>
-                <DialogItem id="2" name="Anna"/>
-                <DialogItem id="3" name="Kolya"/>
-                <DialogItem id="4" name="Lena"/>
-                <DialogItem id="5" name="Vladimir"/>
+                {dialogsData.map(({id, name}) => <DialogItem id={id} name={name}/>)}
             </div>
             <div className={s.messages}>
-                <Message message="Hi" />
-                <Message message="How is you it-kamasutra?" />
-                <Message message="Good" />
+                {messagesData.map(({id, message}) => <Message id={id} message={message}/>)}
             </div>
         </div>
     );
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,6 +2,11 @@ import s from './MyPosts.module.css';
 import Post from './Post/Post';
 
 const MyPosts = () => {
+    const postData = [
+        {id: 1, message: "Hi, how are you?", likesCount: 10},
+        {id: 2, message: "It's my first post", likesCount: 1000},
+    ];
+
     return (
         <div className={s.myposts_block}>
             <h3>my posts</h3>
@@ -10,8 +15,9 @@ const MyPosts = () => {
                 <button className={s.block}>Add post</button>
             </div>
             <div className={s.posts}>
-                <Post message="Hi, how are you?" likesCount="10"/>
-                <Post message="It's my first post" likesCount="1000"/>
+                {postData.map(({message, likesCount}) => 
+                    <Post message={message} likesCount={likesCount}/>
+                )}
             </div>
             
         </div>
