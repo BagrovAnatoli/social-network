f1d8c152ca5786bf30f91c93502eb3a2922614cb
BagrovAnatoli <bagrov.anatoli@gmail.com>
2022-10-14 10:44:22 +0300

    51 lesson

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,26 +1,36 @@
 import s from './Users.module.css';
+import axios from 'axios';
+import userPhoto from '../../assets/images/user.png';
 
 const Users = ({users, follow, unfollow, setUsers}) => {
     if (users.length === 0) {
-        setUsers(
-            [
-                {id: 1,
-                    photoUrl: "https://storge.pic2.me/upload/207/5669d2805eada.jpg",
-                    followed: false, fillName: 'Dmitri', status: 'I`m developer', location: {city: 'Minsk', country: 'Belarus'}},
-                {id: 2,
-                    photoUrl: "https://storge.pic2.me/upload/207/5669d2805eada.jpg",
-                    followed: false, fillName: 'Olga', status: 'So good', location: {city: 'Nizhniy Novgorod', country: 'Russia'}},
-                {id: 3,
-                    photoUrl: "https://storge.pic2.me/upload/207/5669d2805eada.jpg",
-                    followed: true, fillName: 'Nikita', status: 'I love world', location: {city: 'Kiev', country: 'Ukraine'}},
-                {id: 4,
-                    photoUrl: "https://storge.pic2.me/upload/207/5669d2805eada.jpg",
-                    followed: true, fillName: 'Leonid', status: 'Have a nice day', location: {city: 'Drezden', country: 'Germany'}},
-                {id: 5,
-                    photoUrl: "https://storge.pic2.me/upload/207/5669d2805eada.jpg",
-                    followed: true, fillName: 'Chan', status: 'Comon everybody', location: {city: 'Pekin', country: 'China'}},
-            ]
-        );
+
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+
+            setUsers(
+                [...response.data.items]
+            );
+        });
+
+        // setUsers(
+        //     [
+        //         {id: 1,
+        //             photoUrl: "https://storge.pic2.me/upload/207/5669d2805eada.jpg",
+        //             followed: false, fillName: 'Dmitri', status: 'I`m developer', location: {city: 'Minsk', country: 'Belarus'}},
+        //         {id: 2,
+        //             photoUrl: "https://storge.pic2.me/upload/207/5669d2805eada.jpg",
+        //             followed: false, fillName: 'Olga', status: 'So good', location: {city: 'Nizhniy Novgorod', country: 'Russia'}},
+        //         {id: 3,
+        //             photoUrl: "https://storge.pic2.me/upload/207/5669d2805eada.jpg",
+        //             followed: true, fillName: 'Nikita', status: 'I love world', location: {city: 'Kiev', country: 'Ukraine'}},
+        //         {id: 4,
+        //             photoUrl: "https://storge.pic2.me/upload/207/5669d2805eada.jpg",
+        //             followed: true, fillName: 'Leonid', status: 'Have a nice day', location: {city: 'Drezden', country: 'Germany'}},
+        //         {id: 5,
+        //             photoUrl: "https://storge.pic2.me/upload/207/5669d2805eada.jpg",
+        //             followed: true, fillName: 'Chan', status: 'Comon everybody', location: {city: 'Pekin', country: 'China'}},
+        //     ]
+        // );
     }
     
     return (
@@ -28,7 +38,11 @@ const Users = ({users, follow, unfollow, setUsers}) => {
             {users.map(u => <div key={u.id}>
                 <span>
                     <div>
-                        <img className={s.users_photo} alt="ava" src={u.photoUrl}/>
+                        <img
+                            className={s.users_photo}
+                            alt="ava"
+                            src={u.photos.small != null ? u.photos.small : userPhoto}
+                        />
                     </div>
                     <div>
                         {u.followed
@@ -39,12 +53,12 @@ const Users = ({users, follow, unfollow, setUsers}) => {
                 </span>
                 <span>
                     <span>
-                        <div>{u.fullName}</div>
+                        <div>{u.name}</div>
                         <div>{u.status}</div>
                     </span>
                     <span>
-                        <div>{u.location.country}</div>
-                        <div>{u.location.city}</div>
+                        {/* <div>{u.location.country}</div> */}
+                        {/* <div>{u.location.city}</div> */}
                     </span>
                 </span>
             </div>)}
